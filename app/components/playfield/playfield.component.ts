8f3b0ae05fa5e7513722bcd65134e36e1da7c9e5
p-3 <petri.niskanen@propal.se>
2017-04-25 19:36:32 +0200

    added new app icons

--- app/components/playfield/playfield.component.ts
+++ app/components/playfield/playfield.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {EventData} from 'data/observable';
 import {Router} from '@angular/router';
 import {GameService} from '../../services/game.service';
