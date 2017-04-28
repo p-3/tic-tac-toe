e4774a4230dd17146a3af0fc460ef372483cecce
p-3 <petri.niskanen@propal.se>
2017-04-28 18:06:16 +0200

    added new icons and app information

--- app/main.ts
+++ app/main.ts
@@ -3,5 +3,5 @@ import {platformNativeScriptDynamic} from 'nativescript-angular/platform';
 import {enableProdMode} from '@angular/core';
 import {AppModule} from './app.module';
 
-//enableProdMode();
+enableProdMode();
 platformNativeScriptDynamic().bootstrapModule(AppModule);
